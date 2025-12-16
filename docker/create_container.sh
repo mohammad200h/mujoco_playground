5a662322303ba9944b4cd7a03120066bd8bd1c0a
mohammad <mohammad200h@hotmail.com>
2025-12-16 11:09:28 +0000

    added docker and jax GPU install

---
+++ docker/create_container.sh
@@ -0,0 +1,7 @@
+sudo xhost +local:root
+
+sudo docker run --runtime=nvidia -it --name mujocoplayground \
+  -v $(pwd)/../../mujoco_playground:/workspace/mujoco_playground \
+  -e DISPLAY -e LOCAL_USER_ID=$(id -u) -e LOCAL_GID=$(id -g) \
+  -e QT_X11_NO_MITSHM=1 -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+  --net=host  --privileged mujocoplayground
